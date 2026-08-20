76cd843877923582801343b6bc16d27a18bc5a02
emmanuel <emmanuel.munene@student.moringaschool.com>
2026-08-20 23:39:04 +0300

    fix: extend Tailwind content globs to cover root-level features/pages directories

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx}',
+    './features/**/*.{js,jsx}',
+    './pages/**/*.{js,jsx}',
+  ],
   theme: {
     extend: {
       colors: {
